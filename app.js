f7439765ae7625b68ff91a15ad069f0c100228b4
Sodi <tassaduq009@gmail.com>
2025-07-12 03:23:09 +0500

    Hot reload Added to the website

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 'use strict';
-
+const { exec } = require('child_process');
 const express = require('express');
 const path = require('path');
+const livereload = require('livereload');
+const connectLivereload = require('connect-livereload');
+
 const app = express();
 
+// Setup live reload
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -13,4 +28,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    exec(`start http://localhost:${PORT}`); // Windows
 });
